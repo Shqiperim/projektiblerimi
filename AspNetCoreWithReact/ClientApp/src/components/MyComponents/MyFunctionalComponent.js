22e30ba2324364e7bdda933723f56760bce7c27a
shqipi <shqipi_r@hotmail.com>
2022-05-06 20:30:37 +0200

    Set up React props

--- AspNetCoreWithReact/ClientApp/src/components/MyComponents/MyFunctionalComponent.js
+++ AspNetCoreWithReact/ClientApp/src/components/MyComponents/MyFunctionalComponent.js
@@ -1,10 +1,14 @@
 ﻿import React from 'react';
 
-const MyFunctionalComponent = () => {
+const MyFunctionalComponent = (props) => {
     return (
         < div >
             <h1>
                 My Functional Component
+
+                {/*PROPS */}
+                <hr />
+                <h4>Name: <b>{ props.name ? props.name : "Shqiperim1"}</b></h4>
             </h1>
         </div >
       )
